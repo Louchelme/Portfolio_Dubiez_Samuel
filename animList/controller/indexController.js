ef4fd3a0167a4eda9a53e28597de9513de5445ec
calpetaq <calpetaq@etu.univ-grenoble-alpes.fr>
2024-03-20 18:06:47 +0100

    continuation de la recherche

--- animList/controller/indexController.js
+++ animList/controller/indexController.js
@@ -3,16 +3,35 @@
  */
 const view = {
 
-  //BArre de recherche
+  //Barre de recherche
   searchBar: document.getElementById("searchBar"),
+
 };
 
 
-view.searchBar.addEventListener("change", async() => {
+create_card = () => {
+
+  var mainDiv = document.createElement('div').classList.add('col');
+
+  var cardDiv = document.createElement('div');
+  cardDiv.add('card');
+  cardDiv.add('shadow-sm');
+
+  
+}
+
+
+view.searchBar.addEventListener("change", async(evt) => {
+  if (evt.target.value.trim().length === 0) {
+    return
+  }
+  console.log(evt.target.value)
+
+
   fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('http://api.jikan.moe/v4/anime?q=s&sfw')}`)
   .then( async( response ) => {
 
-    
+
     var recherche = await response.json()
     var data = JSON.parse(recherche.contents).data ;
     
@@ -21,8 +40,14 @@ view.searchBar.addEventListener("change", async() => {
     data.forEach( elem => {
       listeAnime.push( new Anime(elem));
     });
+  
+  
+  }).then( () => {
+    create_card
   })
 })
+
+
 
 // https://jikan.moe/
 // https://api.jikan.moe/v4/anime?q=azertyuiop&sfw
