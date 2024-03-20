db9a947869be006b6708ed8cebeb2e45172ea36a
calpetaq <calpetaq@etu.univ-grenoble-alpes.fr>
2024-03-20 16:26:03 +0100

    ajout de la bar de recherche en front

--- animList/controller/index.js
+++
@@ -1,2 +0,0 @@
-//https://jikan.moe/
-// https://api.jikan.moe/v4/anime?q=azertyuiop&sfw
---
+++ animList/controller/indexController.js
@@ -0,0 +1,17 @@
+/**
+ * Objet constant représentant la vue.
+ */
+const view = {
+  // Bouton CE
+  ceBtn: document.querySelector("input[value='CE']"),
+  
+  //Bouton <-
+  RetBtn: document.getElementById('backspace'),
+
+  //Bouton simple (0, 1, 2, 3, ...)
+  simpleBtns : document.querySelectorAll('.bouton_simple'),
+};
+
+
+// https://jikan.moe/
+// https://api.jikan.moe/v4/anime?q=azertyuiop&sfw
